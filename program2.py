30ae4412d3cc3d76b66c0c9f56b0a815dd825f62
Saurav Kumar Yadav <85505274+crims0n-sky@users.noreply.github.com>
2024-05-08 11:00:03 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -9,6 +9,7 @@ def decode_message( s: str, p: str) -> bool:
                         for j in range(len(p)):
                                 if s[i]!= p[j]:
                                         return False
+                                elif p[j]
                                 
 
   
