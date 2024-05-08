dd72a92c5de08c137801487e84a89c85939fdf30
Saurav Kumar Yadav <85505274+crims0n-sky@users.noreply.github.com>
2024-05-08 11:15:59 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -11,15 +11,15 @@ class Solution:
         def dfs(i, j):
             if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j]!= 'L':
                 return
-            grid[i][j] = 'W'  # Mark as visited
-            dfs(i - 1, j)  # Up
-            dfs(i + 1, j)  # Down
-            dfs(i, j - 1)  # Left
-            dfs(i, j + 1)  # Right
+            grid[i][j] = "W"
+            dfs(i - 1, j)  
+            dfs(i + 1, j)  
+            dfs(i, j - 1)  
+            dfs(i, j + 1)  
 
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 'L':
+                if grid[i][j] == "L":
                     dfs(i, j)
                     island_count += 1
         return island_count
