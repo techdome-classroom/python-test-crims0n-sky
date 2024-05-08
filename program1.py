d28093e16c55276b5f5bbac3923de26f6aa38f8c
Saurav Kumar Yadav <85505274+crims0n-sky@users.noreply.github.com>
2024-05-08 11:12:50 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,5 +2,11 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
-                    
+                    grid = [
+    ["L", "L", "L", "L", "W"],
+    ["L", "L", "W", "L", "W"],
+    ["L", "L", "W", "W", "W"],
+    ["W", "W", "W", "W", "W"],
+]
+print(num_islands(grid))  # Output: 1
         return 0
