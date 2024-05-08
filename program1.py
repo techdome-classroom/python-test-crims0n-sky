70575fddb5f66ebab9dd6ea430bb0ba8e1fbfc4d
Saurav Kumar Yadav <85505274+crims0n-sky@users.noreply.github.com>
2024-05-08 11:24:13 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -12,7 +12,8 @@ class Solution:
             if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j]!= 'L':
                 return
             grid[i][j] = 'D'
-            
+            if i>0 and j>0:
+                dfs(i-1, j-1)
 
         for i in range(rows):
             for j in range(cols):
