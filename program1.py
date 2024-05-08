e7271c21752741bc4d06942d5625c13a13bcf911
Saurav Kumar Yadav <85505274+crims0n-sky@users.noreply.github.com>
2024-05-08 11:17:42 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -11,11 +11,7 @@ class Solution:
         def dfs(i, j):
             if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j]!= 'L':
                 return
-            grid[i][j] = "W"
-            dfs(i - 1, j)  
-            dfs(i + 1, j)  
-            dfs(i, j - 1)  
-            dfs(i, j + 1)  
+            grid[i][j] = "W" 
 
         for i in range(rows):
             for j in range(cols):
