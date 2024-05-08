79f11e5e3ca535820b99e9f974f6c3aafbcd81f8
Saurav Kumar Yadav <85505274+crims0n-sky@users.noreply.github.com>
2024-05-08 10:52:38 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -3,7 +3,7 @@ def decode_message( s: str, p: str) -> bool:
 # write your code here
 # return True if the message is decoded successfully, False otherwise
         if s>p:
-                
+                i
 
   
         return False
